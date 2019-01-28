543a46f0d35333debd7f0718ed32370eae610627
Sarah Autumn <neverether@gmail.com>
2019-01-27 21:59:57 -0800

    got run/stop LED indication working for 5xb

--- application.lua
+++ application.lua
@@ -9,27 +9,32 @@ p_5xb = "http://192.168.0.204:5000/api/app/start/5xb?mode=demo&source=web"
 led_pin = 0
 key_pin = 1
 status = gpio.HIGH
+running = false
 gpio.mode(led_pin, gpio.OUTPUT)
 gpio.mode(key_pin, gpio.INPUT, gpio.PULLUP)
 gpio.write(led_pin, status)
 
 function get_from_api()
    http.get(server,'',
-            function(code, data)
-               -- If no response from HTTP, flash the light.
-               if (code < 0) then
-                  print("HTTP request failed")
-                  t_blink:start()
-               else
-                  local tabla = sjson.decode(data)
-                  
-                  for k,v in pairs(tabla) do print(k,v) end
-                  if tabla["app_running"] == true then
-                     t_blink:stop()
-                     gpio.write(led_pin, gpio.LOW)
-                  end--end solid LED setting loop
-                  
-               end--end data/no data loop
+    function(code, data)
+       -- If no response from HTTP, flash the light.
+       if (code < 0) then
+	  print("HTTP request failed")
+	  t_blink:start()
+       else
+	  local tabla = sjson.decode(data)
+	  
+	  for k,v in pairs(tabla) do print(k,v) end
+	  if tabla["app_running"] == true then
+	     t_blink:stop()
+        if tabla["xb5_running"] == true then	 
+				gpio.write(led_pin, gpio.LOW)
+	     else
+				gpio.write(led_pin, gpio.HIGH)
+	     end
+	  end--end solid LED setting loop
+	  
+       end--end data/no data loop
    end)--end data handling function
 end--end get_from_api()
 
@@ -67,5 +72,5 @@ t_blink:register(500, tmr.ALARM_AUTO, blink)
 -- call get function after each 10 second
 -- any code below tmr.alarm only gets run once
 t_api = tmr.create()
-t_api:register(10000, tmr.ALARM_AUTO, get_from_api)
+t_api:register(5000, tmr.ALARM_AUTO, get_from_api)
 t_api:start()
