ef49e20265c01ed0670620347eea6bd033377f9b
Sarah Autumn <neverether@gmail.com>
2019-01-28 21:39:17 -0800

    first real-world test. bugfixes.

--- application.lua
+++ application.lua
@@ -3,15 +3,16 @@
 require "sjson"
 require "gpio"
 
--- set up some globals
+-- set up some endpoints
 server = "http://192.168.0.204:5000/api/app" -- set server URL
 r_5xb = "http://192.168.0.204:5000/api/app/start/5xb?mode=demo"
 s_5xb = "http://192.168.0.204:5000/api/app/stop/5xb"
 
-led_pin = 0
+led_pin = 2
 key_pin = 1
-status = gpio.HIGH
-running = false --stores a local state
+status = gpio.LOW -- start with lamp off
+running = false --stores a local state as a buffer
+api_dtimer = 3 -- debounces blink state 
 
 -- set pin modes
 gpio.mode(led_pin, gpio.OUTPUT)
@@ -21,22 +22,25 @@ gpio.write(led_pin, status)
 function get_from_api()
    http.get(server,'',
    function(code, data)
-       -- If no response from HTTP, flash the light.
+       -- If no response from HTTP after 3 tries, flash the light.
        if (code < 0) then
           print("HTTP request failed")
-	  t_blink:start()
+	  api_dtimer = api_dtimer - 1
+	  if api_dtimer == 0 then
+	     t_blink:start()
+	  end
        else
 	  local tabla = sjson.decode(data)
-	  
+	  api_dtimer = 3 
 	  for k,v in pairs(tabla) do print(k,v) end
 	  -- if the app is runing, turn off the light
 	  if tabla["app_running"] == true then
 	     t_blink:stop()
               if tabla["xb5_running"] == true then	 
-                 gpio.write(led_pin, gpio.LOW)
+                 gpio.write(led_pin, gpio.LOW) -- lamp ON
                  running = true
-              else
-                 gpio.write(led_pin, gpio.HIGH)
+	      elseif tabla["xb5_running"] == false then
+                 gpio.write(led_pin, gpio.HIGH) -- lamp OFF
                  running = false
               end --end switch checking loop
           end --end API running check loop
@@ -55,13 +59,13 @@ function blink() --blink led
    gpio.write(led_pin, status)
 end --end blinky function
 
-debouncer = 3
+debouncer = 2
 
 poll = function() --poll button and do action
    if gpio.read(key_pin) == gpio.LOW then
       debouncer = debouncer - 1
    else
-      debouncer = 3
+      debouncer = 2
    end
    
    if debouncer == 0 then
@@ -74,16 +78,16 @@ poll = function() --poll button and do action
    end
 end
 
--- key polling function
+-- key polling function every .1 sec
 t_poll = tmr.create()
 t_poll:register(100, tmr.ALARM_AUTO, poll)
 t_poll:start()
 
--- blink timer function call
+-- blink timer function call every .5 sec
 t_blink = tmr.create()
 t_blink:register(500, tmr.ALARM_AUTO, blink)
 
--- call get function after each 1 second
+-- HTTP GET function every 1 sec
 t_api = tmr.create()
 t_api:register(1000, tmr.ALARM_AUTO, get_from_api)
 t_api:start()
