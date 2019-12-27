e1a0cb6596d8dbb923ec8b3415e6c8d21b1e78a5
Sarah Autumn <neverether@gmail.com>
2019-12-26 23:55:03 -0800

    nice. added some features

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A small set of Lua scripts for panel_gen and [nodeMCU](https://en.wikipedia.org/
 
 This provides a key and lamp so volunteers can start the [panel_gen](https://github.com/theautumn/panel_gen) demo during tours. The nodeMCU polls the key pin every 100ms, and polls the API every 1 sec. When the key is operated, a POST is made to the API, which starts the call generator. Another keypress turns it off.
 
-![a key and lamp](https://i.imgur.com/2Pi5eqzl.jpg)
+![a key and lamp](https://i.imgur.com/KM1u2Hyl.jpg)
 ![a little enclosure](https://i.imgur.com/UDKUc5Cl.jpg)
 ![tub of butter substitute](https://imgur.com/g2UxMzql.jpg)
 ![different key and lamp](https://imgur.com/ZbwAk1cl.jpg)
--- application.lua
+++ application.lua
@@ -4,10 +4,9 @@ require "sjson"
 require "gpio"
 
 -- set up some endpoints
-server = "http://192.168.0.204:5000/api/app" -- set server URL
+server = "http://192.168.0.204:5000/api/switches/panel" -- set server URL
 run_panel = "http://192.168.0.204:5000/api/app/start/panel?mode=demo&source=key"
 stop_panel = "http://192.168.0.204:5000/api/app/stop/panel?source=key"
-hv_panel = "http://192.168.0.204:5000/api/switches/panel"
 
 LED_PIN = 2
 KEY_PIN = 1
@@ -15,8 +14,8 @@ HIGHVOL_PIN = 5
 status = gpio.HIGH -- flashes lamp momentarily on startup
 running = false --stores a local state as a buffer
 flash_countdown = 4 -- allows for a couple of no response before blink
-desired_traffic  = "" -- traffic volume controlled by secondary key
-last_traffic = "normal"
+desired_traffic  = nil -- traffic volume controlled by secondary key
+last_traffic = ""
 
 -- set pin modes
 gpio.mode(LED_PIN, gpio.OUTPUT)
@@ -25,85 +24,88 @@ gpio.mode(HIGHVOL_PIN, gpio.INPUT, gpio.PULLUP)
 gpio.write(LED_PIN, status)
 
 function get_app_status()
-   http.get(server,'',
-   function(code, data)
-       -- If no response from HTTP after 3 tries, flash the light.
-	if (code < 0) then  -- if response empty
-		print("App status HTTP request failed")
-		if flash_countdown == 0 then  -- if debouncer has run down
-			blinking, mode = t_blink:state()
-				print("Blinking " .. tostring(blinking))
-				if blinking == false then	-- if timer not already running
-					t_blink:start()
-					print("Blink start issued!")
-	  			end
+	print(">>>>>>>  get app status")
+	http.get(server,'',
+	function(code, data)
+		print("evaluating HTTP data")
+		-- If no response from HTTP after 3 tries, flash the light.
+		if (code < 0) then  -- if response empty
+			print("App status HTTP request failed")
+			if flash_countdown == 0 then -- if debouncer has run down
+				blinking, mode = t_blink:state()
+					print("Blinking " .. tostring(blinking))
+					-- if timer not already running
+					if blinking == false then
+						t_blink:start()
+						print("Blink start issued!")
+	  				end
+			else
+				flash_countdown = flash_countdown -1
+				print("Debounce counter: " .. tostring(flash_countdown))
+			end
 		else
-			flash_countdown = flash_countdown -1
-			print("Debounce counter: " .. tostring(flash_countdown))
-		end
-	else
-		flash_countdown = 2
-		local tabla = sjson.decode(data)
---		print("API Running " .. tostring(tabla["app_running"]))
---		print("Panel Running " .. tostring(tabla["panel_running"]))
-
-	-- if api is accessible and blinky is still running, stop error blinky
-	if tabla["app_running"] == true then
-		blinking, mode = t_blink:state()
-		if blinking == true then
-			t_blink:stop()
-			print("Blink *stop* issued!")
-		end
-		if tabla["panel_running"] == true then
-			gpio.write(LED_PIN, gpio.HIGH) -- lamp ON
-			running = true
-		elseif tabla["panel_running"] == false then
-			gpio.write(LED_PIN, gpio.LOW) -- lamp OFF
- 			running = false
-		end --end switch checking loop
-	end --end API running check loop
-
-       end --end data/no data loop
-   end) --end callback function
+			-- if api is accessible and blinky is still running, stop error blinky
+			-- reset flash_countdown, and grab the table
+			flash_countdown = 2
+			switchtable = sjson.decode(data)
+			blinking, mode = t_blink:state()
+			if blinking == true then
+				t_blink:stop()
+				print("Blink *stop* issued!")
+			end
+			-- the API returns a table of tables so we have to use an index [1]
+			if switchtable[1]["running"] == true then
+				gpio.write(LED_PIN, gpio.HIGH) -- lamp ON
+				running = true
+			elseif switchtable[1]["running"] == false then
+				gpio.write(LED_PIN, gpio.LOW) -- lamp OFF
+ 				running = false
+			end --end switchtable checking loop
+		end --end API running check loop
+	end) --end callback function
 end --end get_app_status()
 
 function change_traffic_load(desired_load)
-	http.get(hv_panel,'',
-	function(code, data)
-		if (code < 0) then -- if response empty
-			print("Traffic load HTTP response empty")
-		else
-			local switchtable = sjson.decode(data)
-			current_load = tostring(switchtable[1]["traffic_load"])
-			print("Current load " .. current_load)
-		end --end if
+	print("changing traffic load <<<<<<<<<")
+	if switchtable == nil or switchtable == {} then
+		print("________________switchtable empty")
+		return 
+	end
+	for index, data in ipairs(switchtable) do
+    		print(index)
 
-		if current_load ~= desired_load then
-			print("Changing the business!")
-			http.request(hv_panel,	-- send kercheep a PATCH!
-				'PATCH',
-				'Content-Type: application/json\r\n',
-				'{"traffic_load": "'.. desired_load ..'"}',
-				function(code, data)
-					if (code < 0) then
-						print("oh boy something fucked up")
-					elseif (code == 200) then
-						last_traffic = desired_load
-						print("Business has been changed heck yeah!")
-					end --end if
-				end) -- end PATCH callback function
-		end -- end main IF block
-	end) --end GET callback function
-end
+		for key, value in pairs(data) do
+		print('\t', key, value)
+		end
+	end
+	
+	local current_load = tostring(switchtable[1]["traffic_load"])
+
+	if current_load ~= desired_load then
+		print("Changing the business!")
+		http.request(server,	-- send kercheep a PATCH!
+			'PATCH',
+			'Content-Type: application/json\r\n',
+			'{"traffic_load": "'.. desired_load ..'"}',
+			function(code, data)
+				if (code < 0) then
+					print("oh boy something fucked up")
+				elseif (code == 200) then
+					print("Business has been changed heck yeah!")
+				end --end if
+			end) -- end PATCH callback function
+	end -- end main IF block
+	last_traffic = desired_load
+end --end GET callback function
 
 
 function blink() --blink led
-   if status == gpio.LOW then
-      status = gpio.HIGH
-   else
-      status = gpio.LOW end
+	if status == gpio.LOW then
+		status = gpio.HIGH
+	else
+		status = gpio.LOW end
 
-   gpio.write(LED_PIN, status)
+	gpio.write(LED_PIN, status)
 end --end blinky function
 
 debouncer = 2
@@ -111,31 +113,32 @@ debouncerdeux = 2
 
 poll = function() --poll keys and do actions
 
-   if gpio.read(KEY_PIN) == gpio.LOW then
-      debouncer = debouncer - 1
-   else
-      debouncer = 2
-   end
+	if gpio.read(KEY_PIN) == gpio.LOW then
+		debouncer = debouncer - 1
+	else
+		debouncer = 2
+   	end
 
-   if debouncer == 0 then
-      if running == false then
+   	if debouncer == 0 then
+		if running == false then
 			http.post(run_panel)
-      else
+		else
 			http.post(stop_panel)
-      end
-   end
+      		end
+   	end
 
 	-- check the traffic volume pin
 	if gpio.read(HIGHVOL_PIN) == gpio.HIGH then
 
-		 	desired_load = "normal"
+		desired_load = "normal"
 	else
-			desired_load = "heavy"
+		desired_load = "heavy"
 	end
+
 	-- if the now value is different than before value, make an API call
 	if desired_load ~= last_traffic then
-			print("Traffic load changing to: " .. tostring(desired_load))
-		 	change_traffic_load(desired_load)
+		print("Traffic load changing to: " .. tostring(desired_load))
+		change_traffic_load(desired_load)
  	end
 end
 
